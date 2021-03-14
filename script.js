a42104213380636794f62cdfb4f91e9865afa586
nleyshac <66838038+nleyshac@users.noreply.github.com>
2021-03-14 09:50:02 -0400

    first commit
    Screen one basic structure outline

---
+++ script.js
@@ -0,0 +1,10 @@
+function start() {
+
+    // $("body").css("background-color", "pink");
+}
+
+
+
+
+// Hey jquery, watch for the page to load. Once it's done, trigger the start function.
+$(window).on("load", start); 
