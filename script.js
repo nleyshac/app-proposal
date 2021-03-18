ce68067f7d058064545d80106b62c6a0b66078db
nleyshac <66838038+nleyshac@users.noreply.github.com>
2021-03-18 12:48:45 -0400

    third commit
    css updates, jquery function fixes (slide toggle), navigation position still needs work

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 // function start() {
-
 //     // $("body").css("background-color", "pink");
 // }
 
-$(document).on("click", "#brand", function(){
-
-    $("#brand_info").slideDown("slow");
-    // $("brand_info").siblings().slideUp("fast");
-});
-
-$(document).on("click", "#ethics",function(){
-
-    $("#ethics_info").slideDown("slow");
-    // $("ethics_info").siblings().slideUp("fast");
-});
-
-$(document).on("click", "#certs",function(){
-
-    $("#certs_info").slideDown("slow");
-//     $("certs_info").siblings().slideUp("fast");
-});
-
-$(document).on("click", "#rating",function(){
-
-    $("#rating_info").slideDown("slow");
-//     $("rating_info").siblings().slideUp("fast");
-});
-
-
-
 // Hey jquery, watch for the page to load. Once it's done, trigger the start function.
 // $(window).on("load", start); 
+
+
+
+$(document).on("click", "#brand", function(){
+     $("#brand_info").slideToggle("slow");
+});
+
+$(document).on("click", "#ethics", function(){
+    $("#ethics_info").slideToggle("slow");
+});
+
+$(document).on("click", "#certs", function(){
+    $("#certs_info").slideToggle("slow");
+});
+
+$(document).on("click", "#rating", function(){
+    $("#rating_info").slideToggle("show");
+});
+
+
+
+function closed() {
+    $("p").hide();
+};
+
+$(window).on("load", closed); 
