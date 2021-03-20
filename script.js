9ac836029035570024c7be13ac28f682e7337e7a
nleyshac <66838038+nleyshac@users.noreply.github.com>
2021-03-20 16:54:21 -0400

    fourth commit
    addition of intro page, replaced placeholder image with live camera (using kirupa), overflow applied on product page, general styling updates

--- script.js
+++ script.js
@@ -7,6 +7,22 @@
 
 
 
+// $(document).on("click", "start_btn",function(){
+//     $("#container").fadeIn("slow");
+// });
+
+
+
+
+
+
+
+
+
+
+
+// Product.html Slide Toggles
+
 $(document).on("click", "#brand", function(){
      $("#brand_info").slideToggle("slow");
 });
@@ -23,10 +39,23 @@ $(document).on("click", "#rating", function(){
     $("#rating_info").slideToggle("show");
 });
 
+// Product.html Hide Nav on Scroll 
 
+// $(window).scroll(function(){
+//     if ($(this).scrollTop() >0) {
+//         $("#navmenutwo").fadeOut();
+//     } else {
+//         $("#navmenutwo").fadeIn();
+//     }
+// });
+
+
+
+// Product.html on page load
 
 function closed() {
     $("p").hide();
+    $("#brand_info").show();
 };
 
 $(window).on("load", closed); 
