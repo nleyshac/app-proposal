c4661530ec28776939655ad16ad23160f15eebf9
nleyshac <66838038+nleyshac@users.noreply.github.com>
2021-03-28 18:36:51 -0400

    final commit
    search.html updated with functional carousel, small changes to HTML and CSS.

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// function start() {
-//     // $("body").css("background-color", "pink");
-// }
-
-// Hey jquery, watch for the page to load. Once it's done, trigger the start function.
-// $(window).on("load", start); 
-
-
-
-// $(document).on("click", "start_btn",function(){
-//     $("#container").fadeIn("slow");
-// });
-
-
-
-
 // Product.html Slide Toggles
 
 $(document).on("click", "#brand", function(){
@@ -32,16 +16,6 @@ $(document).on("click", "#rating", function(){
     $("#rating_info").slideToggle("show");
 });
 
-// Product.html Hide Nav on Scroll 
-
-// $(window).scroll(function(){
-//     if ($(this).scrollTop() >0) {
-//         $("#navmenutwo").fadeOut();
-//     } else {
-//         $("#navmenutwo").fadeIn();
-//     }
-// });
-
 
 
 // Product.html on page load
@@ -52,8 +26,3 @@ function closed() {
 };
 
 $(window).on("load", closed); 
-
-
-
-// Explore page
-
